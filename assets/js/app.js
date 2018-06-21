89e1731d546cca581db6571dfc42172da733a102
Joe Ragno <joseph.ragno3@gmail.com>
2018-06-21 12:25:03 -0400

    set up jQuery for the button and started on click event for button

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var animals = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugarglider", "chincilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval",
+               "salamander", "frog"];
+
+function addButtons() {
+  
+}
